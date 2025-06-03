485dd488c371b0ab12c24ef7a86ff3c3c830123c
Awais Mubbashar <awaismubbashar@gmail.com>
2025-06-03 17:52:01 +0500

    Resolve ksp issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,5 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.hilt) apply false
-    id("com.google.devtools.ksp") version "2.1.20-2.0.1" apply false
+    alias(libs.plugins.ksp) apply false
 }
