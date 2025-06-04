f9923bc081f2933db73c8d91539ab72141bd5c86
Awais Mubbashar <awaismubbashar@gmail.com>
2025-06-04 15:27:55 +0500

    Added onLogin() method in viewmodel

--- app/src/main/java/com/example/registrationcompose/viewmodel/RegisterViewModel.kt
+++ app/src/main/java/com/example/registrationcompose/viewmodel/RegisterViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.viewModelScope
 import com.example.registrationcompose.utils.DataStoreUtils
 import com.example.registrationcompose.utils.UserDataStore
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -18,6 +19,37 @@ class RegisterViewModel @Inject constructor(
             DataStoreUtils.saveData(userDataStore.dataStore, "name", name)
             DataStoreUtils.saveData(userDataStore.dataStore, "email", email)
             DataStoreUtils.saveData(userDataStore.dataStore, "password", password)
+        }
+    }
+
+    private fun checkCredentials(
+        email: String,
+        password: String,
+        onResult: (Boolean) -> Unit
+    ) {
+        viewModelScope.launch {
+            val savedEmail = DataStoreUtils.getData(userDataStore.dataStore, "email", "")
+                .first()
+            val savedPassword = DataStoreUtils.getData(userDataStore.dataStore, "password", "")
+                .first()
+
+            val isValid = (email == savedEmail && password == savedPassword)
+            onResult(isValid)
+        }
+    }
+
+    fun onLogin(
+        email: String,
+        password: String,
+        onSuccess: () -> Unit,
+        onError: () -> Unit
+    ) {
+        checkCredentials(email, password) { isValid ->
+            if (isValid) {
+                onSuccess()
+            } else {
+                onError()
+            }
         }
     }
 }
