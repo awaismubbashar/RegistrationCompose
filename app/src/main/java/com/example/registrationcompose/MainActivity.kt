8071c3f8039a14c360b206340500977918e6483e
Awais Mubbashar <awaismubbashar@gmail.com>
2025-06-14 18:53:54 +0500

    Remove unused code

--- app/src/main/java/com/example/registrationcompose/MainActivity.kt
+++ app/src/main/java/com/example/registrationcompose/MainActivity.kt
@@ -1,27 +1,22 @@
 package com.example.registrationcompose
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
-import com.example.registrationcompose.ui.composables.RegisterScreen
-import com.example.registrationcompose.viewmodel.RegisterViewModel
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    private val registerViewModel: RegisterViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            RegisterScreen { name, password, email ->
-                registerViewModel.saveUser(name = name, password = password, email = email)
-                Toast.makeText(this, "Registration Successful", Toast.LENGTH_SHORT).show()
-            }
+            AppNavigation(Modifier.padding(10.dp))
         }
     }
 }
